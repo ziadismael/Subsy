be00e065b9774bdcd4a81587cec995bf82f26e30
Ziad Ismael <ziad.ismael@icloud.com>
2025-08-04 07:00:29 +0300

    Subscription Routes Implementation

--- controllers/subscriptions.controller.js
+++ controllers/subscriptions.controller.js
@@ -13,18 +13,94 @@ export const createSubscription = async (req, res, next) => {
     }
 }
 
+export const getAllSubscriptions = async (req, res, next) => {
+    try {
+        const subscriptions = await Subscription.find();
+        return res.status(200).json({success: true, data: subscriptions});
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const getSubscriptionDetails = async (req, res, next) => {
+    try {
+        if (!req.params.subID) {
+            const error = new Error('No subscription found with that id');
+            error.status = 404;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({_id: req.params.subID});
+        return res.status(200).json({success: true, data: subscriptions});
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
 export const getUserSubscriptions = async (req, res, next) => {
     try {
         // User in req same as token
-        if (req.user._id.toString()!==req.params.subID.toString()) {
+        if (req.user._id.toString()!==req.params.userID.toString()) {
             const error = new Error("You are not the owner of the account.");
             // error.message = (req.user._id + " " + req.params.subID);
             error.status = 401;
             throw error;
         }
 
-        const subscriptions = await Subscription.find({user: req.params.subID});
+        const subscriptions = await Subscription.find({user: req.user._id});
         return res.status(200).send({success: true, data: subscriptions});
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const getUpcomingRenewals = async (req, res, next) => {
+    try {
+        const userSubs = await Subscription.find({user: req.user._id});
+        // const renewals = [];
+        // for (const sub of userSubs) {
+        //     if (sub.status === "Expired" || (sub.renewalDate - Date.now()) <= (7 * 24 * 60 * 60 * 1000))
+        //     {
+        //         renewals.push(sub);
+        //     }
+        // }
+
+        const renewals = userSubs.filter(sub => {
+            const timeUntil = sub.renewalDate - Date.now();
+            return (
+                sub.status === "Expired" ||
+                (timeUntil <= 7 * 24 * 60 * 60 * 1000 &&
+                timeUntil > 0)
+            );
+        });
+        return res.status(200).send({success: true, data: renewals})
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const cancelSubscription = async (req, res, next) => {
+    try {
+        const userSubs = await Subscription.find({user: req.user._id});
+        const activeSubs = userSubs.filter(sub => sub.status === "Active");
+
+        const cancelledSub = activeSubs.find(
+            sub => sub._id.toString() === req.params.subID
+        );
+        if (!cancelledSub) {
+            const error = new Error('No subscription found with that id');
+            error.status = 404;
+            throw error;
+        }
+        // cancelledSub.renewalDate = new Date();
+        cancelledSub.status = "Cancelled";
+        await cancelledSub.save();
+
+        res.status(200).send({success: true, data: cancelledSub});
     }
     catch (error) {
         next(error);
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,16 +1,19 @@
 import { Router } from "express";
 import authorize from "../middlewares/auth.middleware.js";
-import {createSubscription, getUserSubscriptions} from "../controllers/subscriptions.controller.js";
+import {
+    cancelSubscription,
+    createSubscription,
+    getAllSubscriptions, getSubscriptionDetails, getUpcomingRenewals,
+    getUserSubscriptions
+} from "../controllers/subscriptions.controller.js";
 
 const subRouter = Router();
 
-subRouter.get("/", (req, res) => {
-    return res.status(200).send({message : "Get All Subscriptions"});
-});
+subRouter.get("/", authorize, getAllSubscriptions);
 
-subRouter.get("/:subID", (req, res) => {
-    return res.status(200).send({message : "Get Subscription Details"});
-});
+subRouter.get("/upcoming-renewals",authorize, getUpcomingRenewals);
+
+subRouter.get("/:subID", authorize, getSubscriptionDetails);
 
 subRouter.post("/", authorize ,createSubscription);
 
@@ -22,15 +25,11 @@ subRouter.delete("/:subID", (req, res) => {
     return res.status(200).send({message : "DELETE Subscriptions"});
 });
 
-subRouter.get("/user/:subID", authorize ,getUserSubscriptions);
+subRouter.get("/user/:userID", authorize ,getUserSubscriptions);
 
-subRouter.put("/:subID/cancel", (req, res) => {
-    return res.status(200).send({message : "Get Subscription Details"});
-});
+subRouter.put("/:subID/cancel",authorize, cancelSubscription);
 
-subRouter.get("/upcoming-renewals", (req, res) => {
-    return res.status(200).send({message : "Get Upcoming Renewals"});
-});
+
 
 
 export default subRouter;
